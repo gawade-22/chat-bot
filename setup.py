2800bb889684dbd150ec39f7872af983337bf48f
gawade-22 <gawadeprathamesh30@gmail.com>
2026-06-06 17:58:44 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="prathamesh",
+    author_email="gawadeprathamesh30@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
